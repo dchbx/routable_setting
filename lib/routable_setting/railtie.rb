a109d81adfb0ff9aef5d9f531efcaed78d78b835
raghuram <raghuramg83@gmail.com>
2019-03-11 23:35:37 -0400

    Added intializer hook to cache routable settings

--- lib/routable_setting/railtie.rb
+++ lib/routable_setting/railtie.rb
@@ -3,4 +3,8 @@ class RoutableSetting::Railtie < Rails::Railtie
   rake_tasks do
     load 'routable_setting/tasks/routable_setting.rake'
   end
+
+  initializer "routable_setting.rotable_setting_cache" do
+    RoutableSetting::Caches::SettingCache.load!
+  end
 end
